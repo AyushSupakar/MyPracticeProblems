b31448fd21840b4bfa067d5a05557de136843bdf
Ayush Anshuman Supakar <86709199+AyushSupakar@users.noreply.github.com>
2023-07-09 23:50:44 +0530

    Add files via upload

---
+++ p06_LCM.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.util.*;
+
+public class p06_LCM {
+    public static int GCDfun(int a,int b){
+        int c;
+        if(b>a){
+            c= a;
+            a= b;
+            b= c;
+        }
+
+        if(b==0)
+            return a;
+        else
+        return GCDfun(b, a%b);
+    }
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        int a = x.nextInt();
+        int b = x.nextInt();
+        int LCM = (a*b)/(GCDfun(a,b));
+        System.out.println("LCM is " + LCM);
+    }
+}
