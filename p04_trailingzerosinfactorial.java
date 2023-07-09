e3111de3c712479cca8291b3342e3cda9ec296ef
Ayush Anshuman Supakar <86709199+AyushSupakar@users.noreply.github.com>
2023-07-09 16:37:41 +0530

    Add files via upload

---
+++ p04_trailingzerosinfactorial.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.*;
+public class p04_trailingzerosinfactorial {
+    public static void main(String[] args) {
+        int a;
+        Scanner x = new Scanner (System.in);
+        a = x.nextInt();
+        int i=5;
+        int sol=0;
+        while(i<=a){
+            sol = sol + a/i;
+            i=i*5;
+        }
+        System.out.println(sol);
+
+
+    }
+}
