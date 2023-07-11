2cac6d31aa1927c526b0869713559b0aa8506a1c
Ayush Anshuman Supakar <86709199+AyushSupakar@users.noreply.github.com>
2023-07-11 07:22:45 +0530

    Add files via upload

---
+++ p10_AllPrimeSmaller_SieveOfEratosthenes.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.*;
+public class p10_AllPrimeSmaller_SieveOfEratosthenes {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        int p = x.nextInt();
+        boolean[] arr = new boolean[p+1];
+        for(int i=0; i<p;i++){
+            arr[i]=true;
+        }
+        for(int i=2;i<=p*p;i++){
+            for(int j=i*i;j<=p;j=j+i){
+                arr[j]=false;
+            }
+        }
+        for(int i=2;i<=p;i++){
+            if(arr[i]==true)
+            System.out.print(" "+ i);
+        }
+
+
+    }
+}
