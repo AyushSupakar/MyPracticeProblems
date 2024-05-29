f24923184e114c4ded7cb9c7f7346aad71465852
Ayush Anshuman Supakar <supakarayush@gmail.com>
2024-05-29 10:44:31 +0530

    Completed Mathematics

--- P100_TopicWise/Mathematics.java
+++ P100_TopicWise/Mathematics.java
@@ -1,6 +1,25 @@
 package P100_TopicWise;
 
 public class Mathematics {
+
+    //1. Given two integers ‘a’ and ‘m’. The task is to find the smallest modular multiplicative inverse of ‘a’ under
+    // modulo ‘m’. if it does not exist then return -1.
+
+    public static int modInverse(int a, int m)
+    {
+
+        for (int x = 0; x < (m-1); x++) {
+            if(((a*x)%m)==1){
+                return x;
+            }
+
+        }
+        return -1;
+
+    }
+
+
+
     public static void main(String[] args) {
         System.out.println("Hi Maa");
     }
--- P100_TopicWise/Recursion.java
+++ P100_TopicWise/Recursion.java
@@ -1,5 +1,7 @@
 package P100_TopicWise;
 
+
+
 public class Recursion {
     public static void main(String[] args) {
         System.out.println("Hi Maa 2");
