7962a40fb8785d0c0628924dd24f83e116927b57
Ayush Anshuman Supakar <supakarayush@gmail.com>
2024-05-31 21:18:52 +0530

    Finished Recursion and Starting Arrays.

---
+++ P100_TopicWise/Arrays.java
@@ -0,0 +1,23 @@
+package P100_TopicWise;
+
+public class Arrays {
+    public static void main(String[] args) {
+        System.out.println("P100 : Arrays");
+        /*
+        * 1. Arrays : Continous Data Structures. random access : O(1) : a[n]
+        * 2. Fixed size Arrays, Dynamic Sized Arrays. Java - always array
+        * allocation in heap. D - ArrayList. 3 ways to create. al.add.
+        * 3. All ArrayList Methods : al.dd(i,o)/add(o), addAll(C): C= Collection
+        *  : array od objects, clear(), clone(), contains(o), forEach((n)->
+        * {}); remove(i), size(), toArray(),
+        * 4. Arrays. class : Arrays.asList(arr),binarySearch(arr,key); equals
+        * (arr1, arr2), sort(), tooString()
+        * 5. Important Problems :
+        * - Easy : Second Largest Element, if sorted, reverse an array,
+        * - Med : Remove Duplicates, move zeroes to end, rotate array by d elts
+        * - Hard : Leaders in Array (start from last),
+        *
+        *
+        * */
+    }
+}
---
+++ P100_TopicWise/Recursion.java
@@ -0,0 +1,12 @@
+package P100_TopicWise;
+
+public class Recursion {
+    public static void main(String[] args) {
+        //Only Topics :
+        // 1. Tail Recursion and Tail Call Elimination
+        // 2. Josephus Problem
+        // 3. Tower of Hanoi
+        // 4. All Permutations
+        // 5. Rope Cutting Problem
+    }
+}
